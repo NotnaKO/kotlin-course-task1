57d9504d8d07ca1a003eea12f7db4563f5e3b6b3
anton <anton.kopanov@ya.ru>
2025-03-22 01:06:23 +0300

    fixes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,29 +3,30 @@ package org.example
 import kotlinx.coroutines.*
 import java.util.UUID
 
-class Generator {
-    fun generate(template: String, data: List<Map<String, Any>>): Map<UUID, String> {
-        return data.associate {
-            Pair(UUID.randomUUID(), fill(template, it))
-        }
-    }
 
-    private fun fill(template: String, data: Map<String, Any>): String {
-        var result = template
-        data.forEach {
-            result = result.replace("{{${it.key}}}", it.value.toString())
-        }
-
-        return result
-    }
+fun generate(template: String, data: List<Map<String, Any>>): Map<UUID, String> = data.associate {
+    Pair(UUID.randomUUID(), fill(template, it))
 }
+
+
+private fun fill(template: String, data: Map<String, Any>): String {
+    var result = template
+    data.forEach {
+        result = result.replace("{{${it.key}}}", it.value.toString())
+    }
+
+    return result
+}
+
 
 interface PromptClient {
     suspend fun sendRequest(id: UUID, req: String): String
 }
 
 
-class EmulatedClient : PromptClient {
+object EmulatedClient : PromptClient {
+
+    private val chars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
     override suspend fun sendRequest(id: UUID, req: String): String {
         println("Doing sendRequest with id: $id")
         val time = (50..1000).random().toLong()
@@ -35,15 +36,14 @@ class EmulatedClient : PromptClient {
     }
 
     private fun getRandomString(length: Long): String {
-        val chars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
-        return (1..length)
-            .map { chars.random() }
+        return List(length.toInt()) { chars.random() }
             .joinToString("")
     }
 }
 
+data class PromptResult(val id: UUID, val resp: String)
+
 fun main() = runBlocking {
-    val generator = Generator()
     val data = listOf(
         mapOf("name" to "Alice", "subject" to "Kotlin", "time" to 10),
         mapOf("name" to "Bob", "subject" to "Rust", "time" to 20),
@@ -51,14 +51,16 @@ fun main() = runBlocking {
     )
     val template = "Hello, I'm {{name}}, please help me in {{subject}} task. I have {{time}} minutes to answer."
 
-    val prompts = generator.generate(template, data)
+    val prompts = generate(template, data)
     println("Generated Prompts:")
     prompts.forEach { println("${it.key}: ${it.value}") }
 
-    val client = EmulatedClient()
     val results = prompts.map {
         async {
-            Pair(it.key, client.sendRequest(it.key, it.value))
+            PromptResult(
+                id = it.key,
+                resp = EmulatedClient.sendRequest(it.key, it.value)
+            )
         }
     }.awaitAll()
 
